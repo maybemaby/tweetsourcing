f6cc35849c4143d212ce97c8da8928a3f8ff44ac
maybemaby <74226064+maybemaby@users.noreply.github.com>
2021-01-09 14:16:07 -0800

    Repo initiated

---
+++ tweetsourcing/tweets.py
@@ -0,0 +1,33 @@
+import tweepy, requests
+from credentials import *
+
+
+def create_api():
+    auth = tweepy.OAuthHandler(api_key, secret_key)
+    auth.set_access_token(access_token, access_token_secret)
+    return tweepy.API(auth)
+
+
+def retrieve_tweet():
+    tweet_url = input("Enter the URL of the tweet you would like analyzed.\n")
+    tweet_id = tweet_url.split("/status/")[1]
+    return api.get_status(tweet_id)
+
+
+def save_images(tweet_object):
+    tweet_images = tweet_object.entities["media"]
+    image_url = set()
+    for image in tweet_images:
+        image_url.add(image["media_url"])
+        res = requests.get(image["media_url"])
+        res.raise_for_status()
+        image_file = open("image1.jpg", "wb")
+        image_file.write(res.content)
+        image_file.close()
+
+
+if __name__ == "__main__":
+    api = create_api()
+    tweet = retrieve_tweet()
+    tweet_body = tweet.text
+    save_images(tweet)
