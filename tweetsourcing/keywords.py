ff30f7cd4eba183769968a0fdf4bb2dca3eaf3c8
maybemaby <74226064+maybemaby@users.noreply.github.com>
2021-01-10 16:36:26 -0800

    Added docstrings

--- tweetsourcing/keywords.py
+++ tweetsourcing/keywords.py
@@ -2,7 +2,17 @@ from rake_nltk import Rake
 
 
 def extract_kwords(tweet_object):
-    r = Rake(max_length=6)
+    """Uses RAKE algorithm to extract keywords from
+    a tweet.
+
+    Inputs
+    ------
+    Tweepy tweet object
+    Returns
+    -------
+    List of keywords.
+    """
+    r = Rake()
     tweet_text = tweet_object.full_text
     r.extract_keywords_from_text(tweet_text)
     extracted = r.get_ranked_phrases()
@@ -10,5 +20,7 @@ def extract_kwords(tweet_object):
 
 
 def create_query(kword_list):
+    """Takes keyword list and joins them
+    to be used in google search"""
     query = " OR ".join(kword_list)
     return query
