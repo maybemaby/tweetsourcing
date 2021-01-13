912bc3962ea40e6b37db37d6c6f23438ecdbf7ba
maybemaby <74226064+maybemaby@users.noreply.github.com>
2021-01-12 21:15:04 -0800

    Gitignore update and code cleanup.

--- tweetsourcing/main.py
+++ tweetsourcing/main.py
@@ -3,6 +3,7 @@ import keywords
 import googlesearch
 
 if __name__ == "__main__":
+    # Taking a tweet url and generating a query based off the keywords.
     api = tweets.create_api()
     tweet = tweets.retrieve_tweet(api)
     tweets.save_images(tweet)
@@ -10,24 +11,27 @@ if __name__ == "__main__":
     wordlist = keywords.extract_kwords(tweet)
     generated_query = keywords.create_query(wordlist)
     startnum = 1
-    results = googlesearch.kword_search(generated_query, startnum)
-    news = googlesearch.categorize_news(results, wordlist)
+    # Loop for checking best matches in first 50 search results.
     while int(startnum) <= 50:
+        results = googlesearch.kword_search(generated_query, startnum)
+        try:
+            news = googlesearch.categorize_news(results, wordlist, news)
+        except:
+            news = googlesearch.categorize_news(results, wordlist)
         for site_dict in news.values():
             if site_dict["title"] == "":
-                results = googlesearch.kword_search(generated_query, startnum)
-                news = googlesearch.categorize_news(results, wordlist, news)
                 startnum = news["next_page"]
                 break
 
     for k, v in news.items():
-        try:
-            print(
-                f"""
-            Title: {v['title']}
-            Link: {v['link']}
-            Keyword Matches: {v['matches']}
-            """
-            )
-        except TypeError:
-            break
+        if v["title"] != "":
+            try:
+                print(
+                    f"""
+                Title: {v['title']}
+                Link: {v['link']}
+                Keyword Matches: {v['matches']}
+                """
+                )
+            except TypeError:
+                break
